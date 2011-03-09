e860fd9b863f84729621d77cc7c9910796fdafcd
John Peterson <johnpipi@gmail.com>
2011-03-08 19:06:23 -0700

    changed __autoload() to trax_autoload() and then use spl_autoload_register() to make it use the new function.

--- trax/vendor/trax/trax.php
+++ trax/vendor/trax/trax.php
@@ -52,7 +52,7 @@ class Trax {
         $log_path = null,
         $vendor_path = null,
         $public_path = null,
-	$tmp_path = null,
+	    $tmp_path = null,
         $url_prefix = null,
         $url_word_seperator = null, # used to put dashes in the url for seo
         $views_extension = 'phtml',
@@ -61,17 +61,18 @@ class Trax {
         $current_controller_name = null,
         $current_action_name = null,
         $current_controller_object = null,
-	$session_store = "file_store",
-	$session_class_name = "ActiveRecordStore",
-	$session_save_path = "",
-	$session_name = "TRAXSESSID",
-	$session_cookie_domain = null,
-	$session_lifetime = "0",
-	$session_maxlifetime_minutes = "20",
+	    $session_store = "file_store",
+	    $session_class_name = "ActiveRecordStore",
+        $session_save_path = "",
+        $session_name = "TRAXSESSID",
+        $session_cookie_domain = null,
+        $session_lifetime = "0",
+	    $session_maxlifetime_minutes = "20",
         $version = null,
         $show_trax_errors = false,
         $server_default_include_path = null,
-        $include_paths = array();
+        $include_paths = array(),
+        $autoload_prepend = true;
 
     function initialize() {
 
@@ -95,7 +96,7 @@ class Trax {
         self::$log_path          = TRAX_ROOT."/log";
         self::$vendor_path       = TRAX_ROOT."/vendor";
         self::$public_path       = TRAX_ROOT."/public";
-	self::$tmp_path		 = TRAX_ROOT."/tmp";
+	    self::$tmp_path		     = TRAX_ROOT."/tmp";
 
         # Set which file to log php errors to for this application
         # As well in your application you can do error_log("whatever") and it will go to this log file.
@@ -162,7 +163,7 @@ class Trax {
             TRAX_LIB_ROOT,                      # trax libs (vendor/trax or server trax libs)
             PHP_LIB_ROOT,                       # php libs dir (ex: /usr/local/lib/php)
             self::$lib_path,                    # app specific libs extra libs to include
-	    self::$vendor_path,                 # 3rd party libs to include in vendor (vendor/third_party)
+	        self::$vendor_path,                 # 3rd party libs to include in vendor (vendor/third_party)
             self::$server_default_include_path  # tack on the old include_path to the end            
         ));
     }  
@@ -176,7 +177,7 @@ class Trax {
             ActiveRecord::$database_settings = parse_ini_file(self::$config_path."/database.ini", true); 
             #error_log("db settings:".print_r(ActiveRecord::$database_settings, true));
         }
-	ActiveRecord::$environment = TRAX_ENV;        
+	    ActiveRecord::$environment = TRAX_ENV;        
     }
 
     function include_env_config() {
@@ -195,7 +196,8 @@ class Trax {
 ###################################################################
 # Auto include model / controller / other app specific libs files
 ###################################################################
-function __autoload($class_name) {
+spl_autoload_register('trax_autoload', false, Trax::$autoload_prepend);
+function trax_autoload($class_name) {
     $file = Inflector::underscore($class_name).".php";
     $file_org = $class_name.".php";
 
@@ -212,12 +214,6 @@ function __autoload($class_name) {
         # Include users application libs
         include_once(Trax::$lib_path."/$file_org");
     }  
-          
-    # add to the __autoload function from Trax
-    # just define _autoload()
-    if(function_exists('_autoload')) {
-        call_user_func('_autoload', $class_name);
-    }
 }
 
 
